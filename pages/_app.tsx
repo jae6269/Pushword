04a002ed8ba4e2c4a3943bd9ae11628f31d833eb
jae6269 <jae6269@gmail.com>
2024-07-01 21:26:27 +0900

    feat: pwa 설정-1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>PushWord</title>
+        <meta
+          name="description"
+          content="Set personal Keyword push notifications"
+        />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
