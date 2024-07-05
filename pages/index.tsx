41b5cd574f914dbb97ba3cfb88f1f5f66761b8b2
jae6269 <jae6269@gmail.com>
2024-07-05 18:31:30 +0900

    chore: firebase db 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
+import fireStore from '@/firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
+
 export default function Home() {
-  return <div>홈페이지</div>;
+  const testClick = async () => {
+    const query = await getDoc(doc(fireStore, 'User', 'f2HG7YOjJbijFjSQBZ9f'));
+    console.log(query.data());
+  };
+  return (
+    <div>
+      <button type="button" onClick={() => testClick}>
+        가져오기ㅇㅇㅇㄴ
+      </button>
+    </div>
+  );
 }
