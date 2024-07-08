c6c9fd48fea6a5000b0be7e1a3a128f439d5acea
jae6269 <jae6269@gmail.com>
2024-07-09 02:46:20 +0900

    feat: 로고추가

--- components/Sign/SignIn/index.tsx
+++ components/Sign/SignIn/index.tsx
@@ -1,8 +1,17 @@
+import { SignInButton } from '@/components/Button/SignButton';
+import Image from 'next/image';
+import MainLogo from '@/public/images/main-full-logo.png';
+
 import classNames from 'classnames/bind';
 import styles from './SignIn.module.scss';
-
 const cn = classNames.bind(styles);
 
 export default function SignIn() {
-  return <div className={cn('container')}>로그인</div>;
+  return (
+    <div className={cn('container')}>
+      <h1>로그인이 필요합니다.</h1>
+      <Image src={MainLogo} alt="메인 로고" width={400} />
+      <SignInButton />
+    </div>
+  );
 }
--- components/Sign/SignOut/index.tsx
+++ components/Sign/SignOut/index.tsx
@@ -1,8 +1,13 @@
 import classNames from 'classnames/bind';
 import styles from './SignOut.module.scss';
+import { SignOutButton } from '@/components/Button/SignButton';
 
 const cn = classNames.bind(styles);
 
 export default function SignOut() {
-  return <div className={cn('container')}>로그아웃</div>;
+  return (
+    <div className={cn('container')}>
+      <SignOutButton />
+    </div>
+  );
 }
