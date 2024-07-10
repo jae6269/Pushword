c725ec59be66a9c536f351518fddec0cde407cc9
jae6269 <jae6269@gmail.com>
2024-07-11 03:31:41 +0900

    feat: 채널, 서치 페이지 로그인 검증

--- pages/channels/index.tsx
+++ pages/channels/index.tsx
@@ -1,8 +1,22 @@
+import useGetUser from '@/hooks/useGetUser';
+import { useRouter } from 'next/router';
+import { useMenuStore } from '@/store/store';
+import { URL_LIST } from '@/constants/url';
+
 import classNames from 'classnames/bind';
 import styles from './ChannelPage.module.scss';
 
 const cn = classNames.bind(styles);
 
 export default function Channels() {
+  const user = useGetUser();
+  const router = useRouter();
+  const { setMenu } = useMenuStore();
+  if (!user) {
+    alert('로그인이 필요합니다.');
+    router.push(URL_LIST.account);
+    setMenu('account');
+    //확인 : 비로그인시 로그인 화면으로이동하도록 (나중에 alert-> modal로 변경)
+  }
   return <div className={cn('container')}> 채널페이지</div>;
 }
