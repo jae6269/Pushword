8dd6602cb29912701b60effae8ae4600fae0bec5
jae6269 <jae6269@gmail.com>
2024-07-12 01:55:55 +0900

    feat: SearchBar 컴포넌트 연결

---
+++ components/SearchBar/index.tsx
@@ -0,0 +1,17 @@
+import classNames from 'classnames/bind';
+import styles from './SearchBar.module.scss';
+
+const cn = classNames.bind(styles);
+
+interface SearchBarProps {
+  label: string;
+  placeholder: string;
+}
+export default function SearchBar({ label, placeholder }: SearchBarProps) {
+  return (
+    <div className={cn('container')}>
+      <label htmlFor="search">{label}</label>
+      <input type="text" placeholder={placeholder} id="search" />
+    </div>
+  );
+}
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,8 +1,16 @@
 import classNames from 'classnames/bind';
 import styles from './SearchPage.module.scss';
+import SearchBar from '@/components/SearchBar';
 
 const cn = classNames.bind(styles);
 
+const SEARCH_LABEL = '채널 검색';
+const SEARCH_PLACEHOLDER = '채널명을 입력하세요.';
+
 export default function Search() {
-  return <div className={cn('container')}> 검색</div>;
+  return (
+    <div className={cn('container')}>
+      <SearchBar label={SEARCH_LABEL} placeholder={SEARCH_PLACEHOLDER} />
+    </div>
+  );
 }
