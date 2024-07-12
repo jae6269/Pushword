96b0fd277589304cac5f5865936c489fd412928b
jae6269 <jae6269@gmail.com>
2024-07-12 15:16:07 +0900

    feat: 단일 채널 타입 정의

--- types/types.ts
+++ types/types.ts
@@ -4,3 +4,45 @@ export interface UserType {
   displayName: string;
   photoURL: string;
 }
+
+// 유튜브 채널 타입 -----------
+export interface YoutubeChannelType {
+  kind: string;
+  etag: string;
+  id: YoutubeChannelIdType;
+  snippet: YoutubeChannelSnippetType;
+}
+
+export interface YoutubeChannelIdType {
+  kind: string;
+  channelId: string;
+}
+
+export interface YoutubeChannelSnippetType {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: YoutubeChannelThumbnailsType;
+  channelTitle: string;
+  liveBroadcastContent: string;
+  publishTime: string;
+}
+
+export interface YoutubeChannelThumbnailsType {
+  default: YoutubeChannelDefaultImgType;
+  medium: YoutubeChannelMediumImgType;
+  high: YoutubeChannelHighImgType;
+}
+
+export interface YoutubeChannelDefaultImgType {
+  url: string;
+}
+
+export interface YoutubeChannelMediumImgType {
+  url: string;
+}
+
+export interface YoutubeChannelHighImgType {
+  url: string;
+}
