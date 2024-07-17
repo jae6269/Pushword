0fe1bbccf5fb9d6eaf90848ed988e0db8ef12809
jae6269 <jae6269@gmail.com>
2024-07-18 02:16:18 +0900

    feat: errorboundary 설정

---
+++ components/ErrorFallback/index.tsx
@@ -0,0 +1,14 @@
+import { FallbackProps } from 'react-error-boundary';
+
+export default function ErrorFallback({
+  error,
+  resetErrorBoundary,
+}: FallbackProps) {
+  const errorStatus = error.toString().split('code')[1];
+
+  const handleResetButtonClick = () => {
+    resetErrorBoundary();
+  };
+
+  return <div></div>;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+
+import { ToastContainer } from 'react-toastify';
+import { ErrorBoundary } from 'react-error-boundary';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import useGetUser from '@/hooks/useGetUser';
 import BottomNavBar from '@/components/BottomNavBar';
-import { ToastContainer } from 'react-toastify';
+import ErrorFallback from '@/components/ErrorFallback';
 
 import 'react-toastify/dist/ReactToastify.css';
 import '@/styles/reset.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,90 +28,92 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Head>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <title>PushWord</title>
-        <meta
-          name="description"
-          content="Set notifications with your own keywords"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#ffffff" />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Head>
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <title>PushWord</title>
+          <meta
+            name="description"
+            content="Set notifications with your own keywords"
+          />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#ffffff" />
 
-        {/* favicon */}
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+          {/* favicon */}
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.ico" />
 
-        {/* for ios */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="PushWord" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
-        <link
-          rel="apple-touch-startup-image"
-          href="/icon-512x512.png"
-          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-        ></link>
-        <link
-          rel="apple-touch-icon"
-          href="/images/icons/apple/apple-icon.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="/images/icons/apple/apple-icon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/images/icons/apple/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/images/icons/apple/apple-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/images/icons/apple/apple-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/images/icons/apple/apple-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/images/icons/apple/apple-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/images/icons/apple/apple-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/icons/apple/apple-icon-180x180.png"
-        />
-      </Head>
-      <div className="wrapper">
-        <ToastContainer
-          position="top-center"
-          autoClose={1500}
-          hideProgressBar={false}
-          closeOnClick={true}
-          rtl={false}
-          pauseOnHover={false}
-          pauseOnFocusLoss={false}
-        />
-        <Component {...pageProps} user={user} />
-        <BottomNavBar />
-      </div>
+          {/* for ios */}
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-title" content="PushWord" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/icon-512x512.png"
+            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+          ></link>
+          <link
+            rel="apple-touch-icon"
+            href="/images/icons/apple/apple-icon.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/images/icons/apple/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/images/icons/apple/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/images/icons/apple/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/images/icons/apple/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/images/icons/apple/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/images/icons/apple/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/images/icons/apple/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/icons/apple/apple-icon-180x180.png"
+          />
+        </Head>
+        <div className="wrapper">
+          <ToastContainer
+            position="top-center"
+            autoClose={1500}
+            hideProgressBar={false}
+            closeOnClick={true}
+            rtl={false}
+            pauseOnHover={false}
+            pauseOnFocusLoss={false}
+          />
+          <Component {...pageProps} user={user} />
+          <BottomNavBar />
+        </div>
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 }
